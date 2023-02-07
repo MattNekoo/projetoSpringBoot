aabddfc72aa5f7b82aa785a6cdcc939c0c199b86
Mateus Nascimento Lima <100529517+MattNekoo@users.noreply.github.com>
2023-02-07 16:51:28 -0300

    Criando a config e injeção de dados.

--- src/main/java/com/devjava/projeto1/resources/UserResources.java
+++ src/main/java/com/devjava/projeto1/resources/UserResources.java
@@ -1,19 +1,26 @@
 package com.devjava.projeto1.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devjava.projeto1.entities.User;
+import com.devjava.projeto1.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "a", "9", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 }
