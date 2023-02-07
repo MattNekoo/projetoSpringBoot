8eafb6548e03618ebe3d38dc60ebeed7d64640b4
Mateus Nascimento Lima <100529517+MattNekoo@users.noreply.github.com>
2023-02-07 15:19:04 -0300

    Add files via upload

--- src/main/java/com/devjava/projeto1/entities/User.java
+++ src/main/java/com/devjava/projeto1/entities/User.java
@@ -1,5 +1,8 @@
 package com.devjava.projeto1.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,20 +13,31 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
+	private String phone;
+	private String password;
 	
 	@ManyToOne
 	@JoinColumn(name = "department_id")
 	private Department department;
 	
 	public User() {
-		
+	}
+	
+	public User(Long id, String name, String email, String phone, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
 	}
 
 	public Long getId() {
@@ -50,6 +64,22 @@ public class User {
 		this.email = email;
 	}
 
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public Department getDepartment() {
 		return department;
 	}
@@ -57,6 +87,21 @@ public class User {
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}	
 }
---
+++ src/main/java/com/devjava/projeto1/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.devjava.projeto1.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devjava.projeto1.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "a", "9", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
