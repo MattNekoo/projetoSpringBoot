ac24dfc3c4a3ceca1863b782833b5b084778b976
Mateus Nascimento Lima <100529517+MattNekoo@users.noreply.github.com>
2023-02-08 10:19:53 -0300

    Resources e Service - ajustes

--- src/main/java/com/devjava/projeto1/services/UserService.java
+++ src/main/java/com/devjava/projeto1/services/UserService.java
@@ -1,6 +1,7 @@
 package com.devjava.projeto1.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
 	}
 }
