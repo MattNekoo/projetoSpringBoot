1839e32e1da65d8fb0c830efdca72b6ddcaa371f
Mateus Nascimento <mateusnl.vg@gmail.com>
2023-02-10 15:53:28 -0300

    Payment - entidade

--- src/main/java/com/devjava/projeto1/config/TestConfig.java
+++ src/main/java/com/devjava/projeto1/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devjava.projeto1.entities.Category;
 import com.devjava.projeto1.entities.Order;
 import com.devjava.projeto1.entities.OrderItem;
+import com.devjava.projeto1.entities.Payment;
 import com.devjava.projeto1.entities.Product;
 import com.devjava.projeto1.entities.User;
 import com.devjava.projeto1.entities.enums.OrderStatus;
@@ -80,5 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
+
 	}
 }
