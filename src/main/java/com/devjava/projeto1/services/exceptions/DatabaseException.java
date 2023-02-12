8c4bc5cc4fa9314bec51adf448e89fea9b2a24e8
Mateus Nascimento <mateusnl.vg@gmail.com>
2023-02-12 11:16:56 -0300

    Exception handling - delete

---
+++ src/main/java/com/devjava/projeto1/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.devjava.projeto1.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
